ee01418f99db62b003112f5f7e28ac0f7e209027
Joao Esperancinha <jofisaes@hotmail.com>
2015-11-14 15:23:15 -0800

    SZ Initial project setup

---
+++ build.sh
@@ -0,0 +1,25 @@
+echo ---- Creating system variables...
+export GOROOT=/usr/lib/go
+export GOPATH=/opt/go/inner-text-searcher/lib
+export PATH=$PATH:$GOPATH:$GOROOT/bin
+go env
+
+echo ---- Loading commong libraries
+go get "github.com/stretchr/testify/assert"
+echo -------- Loaded github.com/stretchr/testify/assert
+go get "github.com/stretchr/objx"
+echo -------- Loaded github.com/stretchr/objx
+
+echo ---- Creating pkg builds
+export GOPATH=/opt/go/inner-text-searcher/damerau-levenshtein-distance
+cd damerau-levenshtein-distance 
+go install -v -gcflags "-N -l" ./...
+cd ..
+export GOPATH=/opt/go/inner-text-searcher/needleman-wunsch-distance
+cd needleman-wunsch-distance
+go install -v -gcflags "-N -l" ./...
+cd ..
+export GOPATH=/opt/go/inner-text-searcher/smith-waterman-distance
+cd smith-waterman-distance
+go install -v -gcflags "-N -l" ./...
+cd ..
