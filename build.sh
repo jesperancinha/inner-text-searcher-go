f554712055512a20eee2d39787997efbc33b46de
Joao FSE <jofisaes@hotmail.com>
2015-11-16 00:54:59 +0100

    SZ Corrects build script

--- build.sh
+++ build.sh
@@ -11,19 +11,6 @@ go get "github.com/stretchr/objx"
 echo -------- Loaded github.com/stretchr/objx
 
 echo ---- Creating pkg builds
-export GOPATH=/opt/go/inner-text-searcher/damerau-levenshtein-distance
-cd damerau-levenshtein-distance 
-go install -v -gcflags "-N -l" ./...
-cd ..
-export GOPATH=/opt/go/inner-text-searcher/needleman-wunsch-distance
-cd needleman-wunsch-distance
-go install -v -gcflags "-N -l" ./...
-cd ..
-export GOPATH=/opt/go/inner-text-searcher/smith-waterman-distance
-cd smith-waterman-distance
-go install -v -gcflags "-N -l" ./...
-cd ..
-export GOPATH=/opt/go/inner-text-searcher/optimal-sring-alignment-distance
-cd optimal-sring-alignment-distance
+cd string-distance 
 go install -v -gcflags "-N -l" ./...
 cd ..
