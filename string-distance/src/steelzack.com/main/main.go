76cc4f4abb838a4ead8410566e4567be7662027b
Joao FSE <jofisaes@hotmail.com>
2015-11-22 12:28:37 +0100

    SZ changes architecture for testing

--- string-distance/src/steelzack.com/main/main.go
+++ string-distance/src/steelzack.com/main/main.go
@@ -8,10 +8,25 @@ import (
 func main() {
 	optaln := new(algorithms.OptAli)
 	log.Println(optaln.CalculateDistance("SimpleTest", "SimplTest"))
+	
+	optaln = new(algorithms.OptAli)
+	log.Println(optaln.CalculateDistance("ATCGT", "TGGTG"))
+
+	optaln = new(algorithms.OptAli)
+	log.Println(optaln.CalculateDistance("GACTTAC", "CGTGAATTCAT"))
+
 	damleu := new(algorithms.DamLev)
 	log.Println(damleu.CalculateDistanceWithAlphabet("SimpleTest", "SimplTest", 20))
-	neewun := algorithms.NewNeeWun(-2, 1,1)
+	
+	damleu = new(algorithms.DamLev)
+	log.Println(damleu.CalculateDistanceWithAlphabet("ATCGT", "TGGTG", 20))
+
+	//gap, missmatchscore, exactscore
+	
+	neewun := algorithms.NewNeeWun(-2, 1, 1)
 	log.Println(neewun.CalculateDistance("ATCGT", "TGGTG"))
-	log.Println(neewun.CalculateDistance("DIMI", "DAMI"))
+
+	smiwat := algorithms.NewSmiWat(-4, 3, 5)
+	log.Println(smiwat.CalculateDistance("GACTTAC", "CGTGAATTCAT"))
 
 }
